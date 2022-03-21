742f8ba82d81754a4ce8fa5792d6216c8b5a1966
ShahierNashaat <shahiernashaat97@gmail.com>
2022-03-21 22:25:40 +0200

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     get '/messages', to: 'messages#index'
   end
 
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
   root 'static#index'
 end
